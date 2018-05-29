556e2098d41e1feb45aa008a4be00d7c3ef58fdb
hellow10 <39315875+hellow10@users.noreply.github.com>
2018-05-29 14:38:29 -0300

    Add files via upload

--- src/app/model/Hotel.java
+++ src/app/model/Hotel.java
@@ -1,5 +1,6 @@
 package app.model;
 
+import java.io.Serializable;
 import java.util.TreeMap;
 
 public class Hotel {
@@ -12,7 +13,7 @@ public class Hotel {
     private TreeMap<String, Conserje> conserjes = new TreeMap<>();
     private TreeMap<String, Cliente> clientes = new TreeMap<>();
     private TreeMap<String, Reserva> reservas = new TreeMap<>();
-    private Admin admin = new Admin("admin", new Password("password"), "nombre");
+    private Admin admin;
     private double totalIngresos = 0;
 
     /*Constructor privado. Esto impide instanciar la clase libremente*/
@@ -26,5 +27,25 @@ public class Hotel {
 
     public Admin getAdmin() {
         return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public TreeMap<String, Habitacion> getHabitaciones() {
+        return habitaciones;
+    }
+
+    public TreeMap<String, Conserje> getConserjes() {
+        return conserjes;
+    }
+
+    public TreeMap<String, Cliente> getClientes() {
+        return clientes;
+    }
+
+    public TreeMap<String, Reserva> getReservas() {
+        return reservas;
     }
 }
