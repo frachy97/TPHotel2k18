8626e9463f1d3533eedd6abb9997b34bf33c4a6c
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-06-01 16:42:57 -0300

    Add files via upload

--- src/app/Programa.java
+++ src/app/Programa.java
@@ -5,6 +5,7 @@ import app.enums.TipoHab;
 import app.model.*;
 
 import java.io.Console;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -16,14 +17,19 @@ public class Programa {
         Controlador controlador = new Controlador(hotel);
         //controlador.inicio();
         Conserje conserjito = new Conserje("Pepe", new Password("lalala"), "papapapa");
-        Cliente unCliente = new Cliente("pepe", "pepon", "10101010", "Springfield",
-                "lalalaa");
+        Cliente unCliente = new Cliente("pepe", "pepon", "10101010", "Springfield", "lalalaa");
+        Cliente unCliente2 = new Cliente("pablo", "asereje", "11111111", "Springfield", "lalalaa");
         Habitacion habitacion1 = new Habitacion("101", TipoHab.INDIVIDUAL, 1);
         Habitacion habitacion2 = new Habitacion("102", TipoHab.INDIVIDUAL, 1);
         hotel.agregarHabitacion(habitacion1);
         hotel.agregarHabitacion(habitacion2);
         hotel.agregarCliente(unCliente);
+        Reserva r=new Reserva(unCliente,habitacion1, LocalDate.now(), LocalDate.of(2018, 6, 3));
+        hotel.agregarReserva(r);
+        Reserva r2=new Reserva(unCliente2,habitacion1, LocalDate.of(2018,6,5), LocalDate.of(2018, 6, 6));
+        hotel.agregarReserva(r2);
         conserjito.altaReserva(new Scanner(System.in), hotel);
+        
     }
 
 
